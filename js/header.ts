8d2e33081411fd01ad63795a88f9e13628947dff
SuperFeda <112332753+SuperFeda@users.noreply.github.com>
2025-01-03 21:15:47 +0300

    Delete js/header.ts

--- js/header.ts
+++
@@ -1,4 +0,0 @@
-const openHeaderDropdown = (): void => {
-    document.getElementById("adap_header").classList.toggle("active-header")
-    document.getElementById("adap-nav-links").classList.toggle("active-header")
-}
